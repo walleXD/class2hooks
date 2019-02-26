e12f0148bdc8487462efcd5a35b1badf4c3c2246
Walee Ahmed <dev@walee.me>
2019-02-26 08:45:26 -0500

    added basic refactoring boilerplate

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello from class2hooks")
+import { API, FileInfo, Options } from "jscodeshift"
+
+export default (file: FileInfo, api: API, options: Options) => {
+  //   const j = api.jscodeshift
+  //   const root = j(file.source)
+
+  return file.source
+}
