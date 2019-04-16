f8c3b28e5e7fb968fbf1d94385367b6adad73f59
Walee Ahmed <dev@walee.me>
2019-04-15 21:52:04 -0400

    moved to eslint for linting & added explicit types

--- packages/core/src/__tests__/transforms.test.ts
+++ packages/core/src/__tests__/transforms.test.ts
@@ -2,8 +2,9 @@ import { readdirSync, statSync } from "fs"
 import { defineTest } from "lib/testRunners"
 import { join } from "path"
 
-const dirs = (p: string) =>
-  readdirSync(p).filter(f => statSync(join(p, f)).isDirectory())
+const dirs = (p: string): string[] =>
+  readdirSync(p).filter((f): boolean => statSync(join(p, f)).isDirectory())
 
-dirs(join(__dirname, "..", "__testfixtures__"))
-  .map(name => defineTest(__dirname, name))
+dirs(join(__dirname, "..", "__testfixtures__")).map(
+  (name): void => defineTest(__dirname, name)
+)
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,70 +1,42 @@
 import {
-    API,
-    arrowFunctionExpression,
-    ASTNode,
-    blockStatement,
-    FileInfo,
-    identifier,
-    Options,
-    returnStatement,
-    variableDeclaration,
-    variableDeclarator
-  } from "jscodeshift"
-  import { Collection } from "jscodeshift/src/Collection"
-  import runChecks from "lib/runChecks"
-  import { IRuntimeOptions } from "./lib/types"
-  import {
-    findReactES6ClassDeclaration,
-    getClassName,
-    hasOnlyRenderMethod,
-    isRenderMethod,
-    removeReactComponentImport,
-    skipTransformation
-  } from "./lib/utils"
-  
-  /**
-   * Pure Class To Functional Component
-   * =============================
-   * Approach
-   * ---------
-   * - Check if the passed source is refactorable, i.e. looks for things we can't refactor
-   * - If the the initial check passes, then check for where/what we can refactor
-   * - Then run transformations based on the refactorable collections
-   */
-  export default (file: FileInfo, api: API, options: Options) => {
-    const j = api.jscodeshift
-    const root: Collection<ASTNode> = j(file.source)
-  
-    const defaultOptions: IRuntimeOptions = {
-      refactorState: false
-    }
-  
-    const runtimeOptions: IRuntimeOptions = { ...defaultOptions, ...options }
-  
-    const isTransformable: boolean = runChecks(root, runtimeOptions)
-  
-    if (!isTransformable) {
-      skipTransformation(root, "Failed initial Check")
-      return null
-    }
-  
-    runTransformation(root)
-    removeReactComponentImport(root)
-  
-    return root.toSource()
-  }
-  
-  const runTransformation = (path: Collection<ASTNode>) =>
-    findReactES6ClassDeclaration(path) // collection of classes
-      .filter(p => hasOnlyRenderMethod(p)) // makes sure the classes only have render methods
-      .replaceWith(p => {
+  API,
+  arrowFunctionExpression,
+  ASTNode,
+  blockStatement,
+  FileInfo,
+  identifier,
+  Options,
+  returnStatement,
+  variableDeclaration,
+  variableDeclarator,
+  VariableDeclaration
+} from "jscodeshift"
+import { Collection } from "jscodeshift/src/Collection"
+import runChecks from "lib/runChecks"
+import { IRuntimeOptions } from "./lib/types"
+import {
+  findReactES6ClassDeclaration,
+  getClassName,
+  hasOnlyRenderMethod,
+  isRenderMethod,
+  removeReactComponentImport,
+  skipTransformation
+} from "./lib/utils"
+
+const runTransformation = (
+  path: Collection<ASTNode>
+): Collection<VariableDeclaration> =>
+  findReactES6ClassDeclaration(path) // collection of classes
+    .filter((p): boolean => hasOnlyRenderMethod(p)) // makes sure the classes only have render methods
+    .replaceWith(
+      (p): VariableDeclaration => {
         const name = getClassName(p)
-  
+
         const renderMethod = p.value.body.body.filter(isRenderMethod)[0]
         // @ts-ignore
         const renderBody = renderMethod.value.body // TODO: figure out why we are getting type mismatch for renderBody
         const renderReturn = renderBody.body[0].argument
-  
+
         // TODO: Add ability to make implicit returns on JSX
         return variableDeclaration("const", [
           variableDeclarator(
@@ -75,5 +47,37 @@ import {
             )
           )
         ]) // replaces class with an arrow function with same name
-      })
-  
+      }
+    )
+
+/**
+ * Pure Class To Functional Component
+ * =============================
+ * Approach
+ * ---------
+ * - Check if the passed source is refactorable, i.e. looks for things we can't refactor
+ * - If the the initial check passes, then check for where/what we can refactor
+ * - Then run transformations based on the refactorable collections
+ */
+export default (file: FileInfo, api: API, options: Options): string | null => {
+  const j = api.jscodeshift
+  const root: Collection<ASTNode> = j(file.source)
+
+  const defaultOptions: IRuntimeOptions = {
+    refactorState: false
+  }
+
+  const runtimeOptions: IRuntimeOptions = { ...defaultOptions, ...options }
+
+  const isTransformable: boolean = runChecks(root, runtimeOptions)
+
+  if (!isTransformable) {
+    skipTransformation(root, "Failed initial Check")
+    return null
+  }
+
+  runTransformation(root)
+  removeReactComponentImport(root)
+
+  return root.toSource()
+}
