a391c991e533ce8759c2c1a29c63a0d95faf4727
Walee Ahmed <dev@walee.me>
2019-05-02 18:46:10 -0400

    updated linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,20 @@
 module.exports = {
     parser: '@typescript-eslint/parser', 
+    plugins: [
+        "react-hooks"
+    ],
     extends: [
         'plugin:@typescript-eslint/recommended', 
         'prettier/@typescript-eslint', 
         'plugin:prettier/recommended', 
+        "plugin:react/recommended"
     ],
     parserOptions: {
         ecmaVersion: 2018, 
         sourceType: 'module', 
     },
+    rules: {
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn"
+    }
 };
